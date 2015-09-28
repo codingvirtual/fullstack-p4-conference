26dd65324751f51ff83a7c7a985967ab8c14efd1
Greg Palen <codingvirtual@gmail.com>
2015-09-28 16:17:41 -0500

    add various client id's

--- ConferenceCentral_Complete/settings.py
+++ ConferenceCentral_Complete/settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
-ANDROID_CLIENT_ID = 'replace with Android client ID'
-IOS_CLIENT_ID = 'replace with iOS client ID'
+WEB_CLIENT_ID = '15582027881-5n1l2vk5rtoonjd2ic2c0v18qp9k7nb0.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = 'AIzaSyAmWZbFd7t_P128fra2328bLYHu-9-B4-o'
+IOS_CLIENT_ID = 'AIzaSyCmyrJ67inUgEJqvNg3Slfqj9EYKMASSsg'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
