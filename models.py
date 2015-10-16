3d9173bcbdfbab9f57efde897e6cfd950b371ade
Greg Palen <codingvirtual@gmail.com>
2015-10-16 15:38:30 -0500

    add websafekey field to SessionForm

--- models.py
+++ models.py
@@ -37,7 +37,7 @@ class SessionForm(messages.Message):
     typeOfSession   = messages.StringField(5)
     date            = messages.StringField(6)
     startTime       = messages.StringField(7)
-
+    websafeKey      = messages.StringField(8)
 
 class ConflictException(endpoints.ServiceException):
     """ConflictException -- exception mapped to HTTP 409 response"""
